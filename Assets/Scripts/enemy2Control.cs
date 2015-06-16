b36430a4b4d6e6f77981f00cb7c4b5f4f00f3f33
Roy Gero <roy.gero.ii@gmail.com>
2015-06-16 15:58:23 -0400

    Fixed the movement of the second enemy. Need to comment the control code a little bit better but it is a start.

--- Assets/Scripts/enemy2Control.cs
+++ Assets/Scripts/enemy2Control.cs
@@ -6,7 +6,7 @@ public class enemy2Control : MonoBehaviour {
 	private Animator animator;
 	private Vector2 dir;
 	private Random rand;
-	private Vector2 position;
+	private Vector3 position;
 	private float timeEllapsed;
 	private float spriteWidth;
 	private float spriteHeight;
@@ -14,56 +14,67 @@ public class enemy2Control : MonoBehaviour {
 	private float maxY;
 	private float minX;
 	private float minY;
+	private bool startMoving;
 
 	// Use this for initialization
 	void Start () {
-
+		startMoving = false;
 		//Capturing the camera perspective so we can restrict it later
 		float distance = transform.position.z - Camera.main.transform.position.z;
 		Vector3 leftBottom = Camera.main.ViewportToWorldPoint (new Vector3 (0,0,distance));
-		Vector3 rightTop = Camera.main.ViewportToWorldPoint (new Vector3 (1,1,distance));
+		Vector3 rightTop = Camera.main.ViewportToWorldPoint (new Vector3 (1, 1, distance));
 
-
-
-		Debug.Log (minY);
-		Debug.Log (maxY);
-
-
+		//Gets the animator to see when the intro animation is completed.
 		animator = this.GetComponent<Animator> ();
 
+		//Time Ellapsed since we got the new random direction
 		timeEllapsed = 0f;
 
+		//Getting the width and height of the sprite to create the valid bounds of the ship's movement
 		spriteWidth = this.GetComponent<SpriteRenderer> ().bounds.size.x;
 		spriteHeight = this.GetComponent<SpriteRenderer> ().bounds.size.y;
 
+		//Establishing the bounds of the ships movement.
 		maxX = rightTop.x - spriteWidth/2;
 		minY = leftBottom.y + spriteHeight/2;
 		minX = leftBottom.x + spriteWidth/2;
 		maxY = rightTop.y - spriteHeight/2;
 
+		//Establishing the first direction
 		dir = getNewDirection();
 		position = this.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (animator.GetCurrentAnimatorStateInfo (1).IsName ("Idle")) {
-			animator.StopPlayback();
+
+		/*	The way this code segment is going to work is as follows:
+		 * 		Every update cycle, it will look at the startMoving flag
+		 * 		This flag is instantiated as false because I want that intro movement
+		 * 		Once that intro animation has been played, the animation will move into "Idle"
+		 * 		
+		 */
+		if (startMoving) {
 			timeEllapsed += Time.deltaTime;
 			if (timeEllapsed > 1.5f) {
 				dir = getNewDirection ();
 				timeEllapsed = 0f;
-			} else {
-				if (position.x <= minX || position.x >= maxX) {
-					dir.x *= -1;
-				}
-				if (position.y <= minY || position.y >= maxY) {
-					dir.y *= -1;
-				}
-				position.x += dir.x * Time.deltaTime;
-				position.y += dir.y * Time.deltaTime;
 			}
+			if (position.x <= minX || position.x >= maxX) {
+				dir.x *= -1;
+			}
+			if (position.y <= minY || position.y >= maxY) {
+				dir.y *= -1;
+			}
+			position.x += dir.x * Time.deltaTime;
+			position.y += dir.y * Time.deltaTime;
 			this.transform.position = position;
+			Debug.Log (this.transform.position);
+		} else {
+			if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Idle")) {
+				startMoving = true;
+				Destroy (animator);
+			}
 		}
 	}
 
